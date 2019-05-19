f539aaa8cc0a6524803ccd58b126bab255dcf8d7
gilesv <gilesv@protonmail.com>
2019-05-19 15:57:32 -0300

    Stream service

--- producer/index.js
+++ producer/index.js
@@ -1,11 +1,41 @@
 const amqp = require('amqplib/callback_api');
+const mongoose = require('mongoose');
 
 const CONFIG = {
   RABBIT_HOST: 'amqp://rabbit',
   QUEUE: 'bus_ping'
 };
 
-function main() {
+const BusPing = mongoose.model('bus_ping', new mongoose.Schema(
+    { 
+      'Unidad' : Number,
+      'nombre': String,
+      'matricula': String,
+      'Instante': String,
+      'Estado': Number,
+      'Comunica': Number,
+      'CoordX': Number,
+      'CoordY': Number,
+      'Linea': String,
+      'Ruta': String,
+      'Posicion': String,
+      'Viaje': String,
+      'Velocidad': String
+    }, { collection: 'bus_ping' }
+  )
+); 
+
+async function main() {
+  mongoose.connect('mongodb://mongodb/buslightyear', function(err) {
+    if (err) {
+      console.log("error in connection: ", err);
+      return;
+    }
+    console.log("connected successfully!");
+  });
+
+  let buses = await BusPing.find({}, 'Unidad').distinct('Unidad');
+
   amqp.connect(CONFIG.RABBIT_HOST, function(error0, connection) {
     if (error0) {
       throw error0;
@@ -18,12 +48,21 @@ function main() {
       channel.assertQueue(CONFIG.QUEUE, {
         durable: false
       });
-  
+
       setInterval(() => {
-        const msg = "test";
-        channel.sendToQueue(CONFIG.QUEUE, Buffer.from(msg));
-        console.log(`[x] Sent: ${msg}`);
-      }, 3000);
+        buses.forEach(async (unidad_bus) => {
+          const first = await BusPing.findOne({ 'Unidad': unidad_bus, $or: [ {'sent': false}, {'sent': { $exists: false }}] });
+
+          if (first) {
+            const msg = JSON.stringify(first);
+            channel.sendToQueue(CONFIG.QUEUE, Buffer.from(msg));
+
+            BusPing.deleteOne({ _id: first._id });
+            await first.save();
+          }          
+        });
+        console.log(`[x] Sent bus ping!`);
+      }, 10000);
     });
   });
 }
